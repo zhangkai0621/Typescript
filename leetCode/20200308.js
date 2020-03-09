0c682127c57cf0859991c62619987035b785a880
ZhangKai <45391737+zhangkai0621@users.noreply.github.com>
2020-03-09 08:41:44 +0800

    修改题目简介

--- leetCode/20200308.js
+++ leetCode/20200308.js
@@ -1,12 +1,17 @@
 /*
- * @Description: 
+ * @Description: 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
  * @Author: zhangkai
  * @Date: 2020-03-08 19:41:07
- * @LastEditTime: 2020-03-08 20:26:26
+ * @LastEditTime: 2020-03-08 20:28:46
  * @LastEditors: zhangkai
  */
 // 两数之和
 // 时间复杂度 O(n^2)
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length; j++) {
